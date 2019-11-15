b3cbb1cd01901856b3a45e1658697a383fe375af
SHARATH CHANDRA <dsharathc7@gmail.com>
2019-11-15 20:09:22 +0000

    Reverse words and the characters and returning at a time

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
-
+import(
+   "fmt"
+   "strings"
+)
 func main() {
-  fmt.Println("Hello World")
+
+
+fmt.Println(reverse("laptop omg"))
+
 }
+func reverse(str string) (string ,string){
+
+
+chars := []rune(str)
+words := strings.Fields(str)
+for i,j :=0, len(words)-1; i<j;i, j=i+1, j-1{
+chars[i],chars[j]= chars[j],chars[i]
+words[i], words[j] = words[j], words[i]
+}
+ return strings.Join(words, " "), string(chars)
+
+ }
+
+  
+
